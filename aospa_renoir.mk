e49ec5179acf388684fb71d26939f06a61013373
EndCredits <endcredits@crepuscular-aosp.icu>
2023-06-25 07:54:12 +0800

    renoir: Initial bring up for Paranoid Android
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- aospa_renoir.mk
+++ aospa_renoir.mk
@@ -11,29 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from renoir device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
-# Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
-
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2101K9G
-PRODUCT_NAME := derp_renoir
+PRODUCT_NAME := aospa_renoir
 
 # DerpFest Stuffs
 TARGET_BOOT_ANIMATION_RES := 1080
-DERP_RELEASE := true
-DERP_BUILDTYPE := Official
-USE_LEGACY_BOOTANIMATION := true
 
 PRODUCT_SYSTEM_NAME := renoir_global
 PRODUCT_SYSTEM_DEVICE := renoir
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="renoir_global-user 13 TKQ1.220829.002 V14.0.4.0.TKIMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-BUILD_FINGERPRINT := Xiaomi/renoir_global/renoir:13/TKQ1.220829.002/V14.0.4.0.TKIMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
